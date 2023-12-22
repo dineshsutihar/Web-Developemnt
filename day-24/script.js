fd87f5d8cd588dbf51aec1fccce660f9403a5c4c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-22 18:11:34 +0530

    completed sellWeapon function

--- day-24/script.js
+++ day-24/script.js
@@ -97,12 +97,35 @@ function buyHealth(){
 }
 
 function buyWeapon(){
-    if(gold >= 30){  
-        gold -= 30; 
-        currentWeapon++;
+    if (currentWeapon<weapons.length-1){
+        if(gold >= 30){  
+            gold -= 30; 
+            currentWeapon++;
+            goldText.innerText=gold;
+            let newWeapon = weapons[currentWeapon].name;
+        text.innerText="You now have a new "+newWeapon+".";
+        inventory.push(newWeapon);
+        text.innerText+=" In your inventory you have: " + inventory;
+        }else{
+            text.innerText="You do not have enough gold to buy a weapon.";
+        }
+    } else {
+        text.innerText="You already have the most powerful weapon!";
+        button2.innerText="Sell weapon for 15 gold";
+        button2.onclick=sellWeapon;
+    }
+}
+
+function sellWeapon(){
+    if(inventory.length>1){
+        gold+=15;
         goldText.innerText=gold;
-        let weapon = weapons[currentWeapon].name;
-    text.innerText="You now have a new weapon.";
+        let currentWeapon=inventory.shift();
+        text.innerText="You sold a "+currentWeapon+".";
+        text.innerText+=" In your inventory you have: " + inventory;
+
+    }else{
+        text.innerText="Don't sell your only weapon!"
     }
 }
 
