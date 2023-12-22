fbefac32a8a7614553f5d012b640f398d1bdcdd9
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-22 15:23:59 +0530

    [JS] added functions

--- day-24/script.js
+++ day-24/script.js
@@ -5,15 +5,31 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-const button1=document.querySelector("#button1");
-const button2=document.querySelector("#button2");
-const button3=document.querySelector("#button3");
-const text=document.querySelector("#text");
-const xpText=document.querySelector("#xpText");
-const healthText=document.querySelector("#healthText");
-const goldText=document.querySelector("#goldText");
-const monsterStats=document.querySelector("#monsterStats");
-const monsterName=document.querySelector("#monsterName");
-const monsterHealthText=document.querySelector("#monsterHealth");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
 
 // Initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+// Initialize functions
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+}
+
+function goCave() {
+  button2.innerText = "Buy weapon (30 gold)";
+}
+
+function fightDragon() {
+  button3.innerText = "Go to town square";
+}
