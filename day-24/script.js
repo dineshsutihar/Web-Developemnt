0997aa8a6e3f66c067b7520b0ec98d809d7d9a9c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-22 15:06:24 +0530

    [JS] added some variables

---
+++ day-24/script.js
@@ -0,0 +1,8 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+const button1=document.querySelector("#button1");
