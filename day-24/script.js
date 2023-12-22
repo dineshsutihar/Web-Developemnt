1fce914c0865d756cc480a119868dcc397c09579
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-22 17:44:48 +0530

    initialized and completed multiple functions

--- day-24/script.js
+++ day-24/script.js
@@ -15,6 +15,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons=[{
+    name: "stick",
+    power: 5,
+
+},  
+{
+    name:"dagger",
+    power: 30,
+},
+{
+    name:"claw hammer",
+    power: 50,
+},
+{
+    name:"sword",
+    power: 100,
+},   
+];
 
 const locations =[{
     name: "town square",
@@ -27,6 +45,12 @@ const locations =[{
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth,buyWeapon,goTown],
     text: "You enter the store.",
+},
+{
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime,fightBeast,goTown],
+    text: "You enter the cave. You see some monsters.",
 }];
 
 // Initialize buttons
@@ -50,11 +74,11 @@ function goTown(){
 }
 
 function goStore() {
-   
+   update(locations[1]);
 }
 
 function goCave() {
-  button2.innerText = "Buy weapon (30 gold)";
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -62,10 +86,31 @@ function fightDragon() {
 }
 
 function buyHealth(){
-
+    if (gold >= 10) {
+        gold -= 10;
+        health +=10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    }else{
+        text.innerText= "You do not have enough gold to buy health."
+    }
 }
 
 function buyWeapon(){
-    
+    if(gold >= 30){  
+        gold -= 30; 
+        currentWeapon++;
+        goldText.innerText=gold;
+        let weapon = weapons[currentWeapon].name;
+    text.innerText="You now have a new weapon.";
     }
+}
+
+function fightSlime(){
+
+}
+
+function fightBeast()  {
+
+}
 
