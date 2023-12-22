58975c0cce511edf4c35f3f5a983d3726108d460
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-22 16:02:23 +0530

    [js] Update button functions and text display in script.js

--- day-24/script.js
+++ day-24/script.js
@@ -39,10 +39,10 @@ function update(location) {
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
 }
 
 function goTown(){
