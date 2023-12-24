299c68e55893b73c751b30acdfbb4cdd2056ab6b
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-24 14:18:52 +0530

    Added come components

---
+++ day-25/script.js
@@ -0,0 +1,23 @@
+const calorieCounter= document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown= document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
+let isError = false;
+function cleanInputString(str) {
+    // Using regrular epression 
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+  }
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    let entryNumber = targetInputContainer.querySelectorAll();
+}
