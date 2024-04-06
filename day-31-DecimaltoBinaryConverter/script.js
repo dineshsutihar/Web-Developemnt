04f0e092994958088d02561e98e3859e751d787a
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-04-06 13:27:49 +0530

    Add: Decimal to Binary - add the Decimal to binary function which return the binary form of decimal value

---
+++ day-31-DecimaltoBinaryConverter/script.js
@@ -0,0 +1,33 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+function showAnimation(){
+
+}
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
