449f88182aa1581ef55e6b48594e05eb22b8d7bf
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-18 14:21:45 +0530

    Skeleton of SpreadSheet is complete using Javascript forEach loo

---
+++ day-36-Spreadsheet/script.js
@@ -0,0 +1,25 @@
+const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
+const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  }
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+
+    })
+  })
+}
